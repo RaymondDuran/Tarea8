ccde1497b48e16cb27fc147d93416054393a9529
RaymondDuran <202010148@itla.edu.do>
2022-11-08 01:11:06 -0400

    First

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { RecordingData, VoiceRecorder } from 'capacitor-voice-recorder';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  recording = false;
+  storedFileNames = [];
 
-  constructor() {}
+  constructor(public photoService: PhotoService) { }
+
+  // CUESTION MODAL
+  isModalOpen = false;
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  // CUESTION FOTOS-CAMARA
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  ngOnInit() {
+    this.loadFiles();
+    VoiceRecorder.requestAudioRecordingPermission();
+  }
+
+  async loadFiles() {
+    Filesystem.readdir({
+      path: '',
+      directory: Directory.Data
+    }).then(result => {
+      console.log(result);
+      this.storedFileNames = result.files;
+    })
+  }
+
+  startRecording() {
+    if (this.recording) {
+      return;
+    }
+    this.recording = true;
+    VoiceRecorder.startRecording();
+  }
+
+  stopRecording() {
+    if (!this.recording){
+      return;
+    }
+    VoiceRecorder.stopRecording().then(async (result: RecordingData) => {
+      if( result.value && result.value.recordDataBase64){
+        const recordData = result.value.recordDataBase64;
+        const fileName = new Date().getTime() + '.wav';
+        await Filesystem.writeFile({
+          path: fileName,
+          directory: Directory.Data,
+          data: recordData
+        });
+        this.loadFiles();
+      }
+    })
+  }
 
 }
+
+
